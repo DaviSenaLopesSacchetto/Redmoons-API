fbe1957915ec1cb318301cb9fffebaf3dd25c647
Davi Sena <senasacchetto@gmail.com>
2025-09-21 19:44:31 -0300

    Atualizada API para classificar 3 classes: CONFIRMED, CANDIDATE, FALSE POSITIVE

--- predict.py
+++ predict.py
@@ -4,6 +4,12 @@ import pandas as pd
 from fastapi import FastAPI
 
 model = joblib.load("models/random_forest.pkl")
+label_map = {
+    0: "CONFIRMED",
+    1: "CANDIDATE",
+    2: "FALSE POSITIVE"
+}
+
 
 app = FastAPI()
 
@@ -17,6 +23,7 @@ def predict(koi_prad : float, koi_period : float, koi_steff : float, koi_srad : 
     # Exemplo: planeta com raio=1.2, período=50 dias, estrela com T=5800K e raio=1.0
     X_new = np.array([[koi_prad, koi_period, koi_steff, koi_srad]])
     pred = model.predict(X_new)
-    print("Previsão:", "CONFIRMED" if pred[0] == 1 else "NÃO CONFIRMADO")
-    return {"prediction": int(pred)}
+    class_name = label_map[pred[0]]
+    print("Prediction: ", class_name)
+    return {"prediction": class_name}
 
